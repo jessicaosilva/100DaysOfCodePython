2ac2323dd0498696052a6c599a448fcb50d4b2a3
Jessica Oliveira da Silva <Jessyolie3@gmail.com>
2022-02-11 17:46:23 -0300

    Day 2 - Exercise 2 - BMI Calculator - Done

--- day2/test2/main.py
+++ day2/test2/main.py
@@ -4,3 +4,7 @@ weight = input("enter your weight in kg: ")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+altura  = float(height)
+peso  = float(weight)
+imc = int(peso /(altura**2))
+print(imc)
