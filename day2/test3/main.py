2a52488f383f1e70d9c244b736c020ee0b9039fe
Jessica Oliveira da Silva <Jessyolie3@gmail.com>
2022-02-11 18:02:29 -0300

    Day 2 - Exercise 3 - Life in Weeks - Done

--- day2/test3/main.py
+++ day2/test3/main.py
@@ -3,3 +3,11 @@ age = input("What is your current age?")
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+
+age_as_int = int(age)
+anos_restantes = 90 - age_as_int
+dias_restantes = anos_restantes * 365
+semanas_restantes = anos_restantes * 52
+meses_restantes = anos_restantes * 12
+
+print(f"You have {dias_restantes} days, {semanas_restantes} weeks, and {meses_restantes} months left.")
